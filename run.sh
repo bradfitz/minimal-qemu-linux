723902212c403501ef1ee64e607aa548cc94ba68
Brad Fitzpatrick <brad@danga.com>
2018-08-02 09:10:07 -0700

    initrd for uroot, and start of networking

--- run.sh
+++ run.sh
@@ -8,10 +8,12 @@ set -x
 qemu-system-x86_64 \
     -device virtio-serial \
     -device virtconsole \
+    -device virtio-net,netdev=net0 \
+    -netdev user,id=net0 \
     -nographic \
     -display none \
-    -net none \
     -m 1024 \
     -kernel $HOME/hack/linux/arch/x86/boot/bzImage  \
+    -initrd "/tmp/initramfs.linux_amd64.cpio" \
     -no-reboot \
-    -append "console=ttyS0,115200 panic=-1"
+    -append "console=ttyS0,115200 panic=-1 ip=dhcp"
